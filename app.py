7b3a29ccbd80ce80691a1cf7be4df78ed6ca63cf
tchggyyy <32212877+tchggyyy@users.noreply.github.com>
2022-11-01 11:14:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def stockplot():
 
 
 if __name__=="__main__":
-    app.run(debug=True, port=5001)
-
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+	
+    #app.run(debug=True, port=5001)
